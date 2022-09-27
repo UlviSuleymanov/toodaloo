477c5130438d905270a26433ce04b79862b9296c
Ülvi Süleymanov <100337078+UlviSuleymanov@users.noreply.github.com>
2022-09-27 12:36:34 +0400

    Revert "Ready to deploy"
    This reverts commit 76441c513938e729a9f0a5b6fef269d37df55453.

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -2,32 +2,28 @@ import React from 'react';
 
 const About = () => {
   return (
-    <div className="about">
-      <div className="tech">
-        <h1>This project is made by Ulvi Suleymanov</h1>
-        <h3>List of technologies I used on this project.</h3>
-        <ul>
-          <li>React-hooks (including custom hooks)</li>
-          <li>Axios</li>
-          <li>Pagination</li>
-          <li>React-Transitions</li>
-          <li>React-Router</li>
-        </ul>
-      </div>
-      <div className="contact">
-        <h2>Contact Info</h2>
-        <ol>
-          <li>
-            <a href="https://github.com/UlviSuleymanov">GitHub</a>
-          </li>
-          <li>
-            <a href="https://www.linkedin.com/in/%C3%BClvi-s%C3%BCleymanov-9a96a5247/">Linkedin</a>
-          </li>
-          <li>
-            <a href="https://t.me/UlviSuleymanov">Telegram</a>
-          </li>
-        </ol>
-      </div>
+    <div>
+      <h1 style={{ marginBottom: 15 }}>This project is made by Ulvi Suleymanov</h1>
+      <h3>List of technologies I used on this project.</h3>
+      <ul>
+        <li>React-hooks (including custom hooks)</li>
+        <li>Axios</li>
+        <li>Pagination</li>
+        <li>React-Transitions</li>
+        <li>React-Router</li>
+      </ul>
+      <h2>Contact Info</h2>
+      <ol>
+        <li>
+          <a href="https://github.com/UlviSuleymanov">GitHub</a>
+        </li>
+        <li>
+          <a href="https://www.linkedin.com/in/%C3%BClvi-s%C3%BCleymanov-9a96a5247/">Linkedin</a>
+        </li>
+        <li>
+          <a href="https://t.me/UlviSuleymanov">Telegram</a>
+        </li>
+      </ol>
     </div>
   );
 };
